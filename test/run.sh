464887b7438c25c4f2f3d5aeaea1153fa758950b
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2022-05-08 00:01:59 -0400

    Add test/example project

---
+++ test/run.sh
@@ -0,0 +1,8 @@
+rm -r bin/
+mkdir bin/
+../bin/evscript -o bin/script.asm script.evs
+rgbgfx -o bin/font.2bpp font.png
+rgbasm -o bin/test.o test.asm
+rgblink -n bin/test.sym -m bin/test.map -o bin/test.gb bin/test.o
+rgbfix -flhg bin/test.gb
+open bin/test.gb
---
+++ test/test.asm
@@ -0,0 +1,75 @@
+SECTION "Header", ROM0[$100]
+Entry:
+	di
+	jp Main
+	ds $150 - @, 0
+
+SECTION "Main", ROM0
+Main:
+	ldh a, [$FF44]
+	cp a, 144
+	jr c, Main
+	xor a, a
+	ldh [$FF40], a
+	ld a, %11111100
+	ldh [$FF47], a
+
+	ld bc, Font.end - Font
+	ld de, Font
+	ld hl, $8000 + 16 * 32
+.copy
+	ld a, [de]
+	inc de
+	ld [hli], a
+	dec c
+	jr nz, .copy
+	dec b
+	jr nz, .copy
+
+	ld de, wScriptPool
+	ld hl, TestScript
+	call ExecuteScript
+	ld a, %10010001
+	ldh [$FF40], a
+.forever
+	jr .forever
+
+SECTION "Print Function", ROM0
+PrintFunction:
+	push hl
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld de, $9800 + 32
+.next
+	ld a, [hli]
+	and a, a
+	jr z, .exit
+	ld [de], a
+	inc de
+	jr .next
+
+.exit
+	pop hl
+	inc hl
+	inc hl
+	ret
+
+SECTION "Font", ROM0
+Font:
+	INCBIN "bin/font.2bpp"
+.end
+
+DEF swap_bank EQUS "ld [$2000], a"
+INCLUDE "../src/runtime/evsbytecode.asm"
+INCLUDE "../src/runtime/evsbytecode16.asm"
+INCLUDE "../src/runtime/driver.asm"
+	; After including the driver, define the jump table.
+	std_bytecode
+	std_bytecode16
+	dw PrintFunction
+
+INCLUDE "bin/script.asm"
+
+SECTION "Script Pool", WRAM0
+wScriptPool: ds 16
